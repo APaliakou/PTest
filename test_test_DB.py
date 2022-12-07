ac2536c9998e797148fee48c70ddfb6ac2f87121
Aliaksandr Paliakou <krakoz9Ibr@gmail.com>
2022-12-07 01:21:34 +0100

    Revert "7654"
    This reverts commit dfec292d38409341d48981b3d3bf643b632dfe77.

--- test_test_DB.py
+++ test_test_DB.py
@@ -1,27 +1,13 @@
 import pymssql
 
-import pymssql
-
 from checker import *
-<<<<<<< Updated upstream
 conn = pymssql.connect(server='172.21.32.1', user='T1000', password='qwerty',
-=======
-
-# conn = pyodbc.connect('Driver={SQL Server};'
-#                      'Server=EPPLWARW0123\SQLEXPRESS;'
-#                      'Database=TRN;'
-#                      'Trusted_Connection=yes;')
-
-
-conn = pymssql.connect(server='127.0.0.1', user='T1000', password='qwerty',
->>>>>>> Stashed changes
                        database='TRN', port='53424')
 
 
 def test_column_completeness_department_name_departments():
-    assert verify_completeness(
-        collect_result("hr.departments", "department_name", conn)), "department_name column is not " \
-                                                                    "complete "
+    assert verify_completeness(collect_result("hr.departments", "department_name", conn)), "department_name column is not " \
+                                                                                        "complete "
 
 
 def test_verify_department_FK():
@@ -41,25 +27,26 @@ def test_verify_department_FK():
 
 
 def test_min_value_length_postalcode_locations():
-    assert verify_min_length(collect_result("hr.locations", "postal_code", conn), 2), \
+    assert verify_min_length(collect_result("hr.locations", "postal_code", conn), 2),\
         f'PostalCode value is shorter than 2 '
 
 
 def test_allowed_values_region_id_employees():
-    assert verify_allowed_values(collect_result("hr.regions", "region_id", conn), [1, 2, 3, 4]), \
+    assert verify_allowed_values(collect_result("hr.regions", "region_id", conn), [1, 2, 3, 4]),\
         f'Status contains values that are not expected (1, 2, 3, 4)'
 
 
 def test_min_value_length_phone_number_employees():
-    assert verify_min_length(collect_result("hr.employees", "phone_number", conn), 2), \
+    assert verify_min_length(collect_result("hr.employees", "phone_number", conn), 2),\
         f'Owner value is shorter than 2 '
 
 
 def test_counts_region_id():
-    assert verify_counts(collect_result("hr.regions", "region_id", conn), 4), \
+    assert verify_counts(collect_result("hr.regions", "region_id", conn), 4),\
         f'Count of UnitMeasureCode does not equals to 4'
 
 
 def test_uniqueness_jobTitle():
-    assert verify_uniqueness(collect_result("hr.jobs", "job_title", conn)), \
+    assert verify_uniqueness(collect_result("hr.jobs", "job_title", conn)),\
         f'job_title values are not unique'
+
